3da66f0919263a30409687887c2c3473befdbab5
HiroUrata <1712026@i.softbank.jp>
2021-11-13 22:25:32 +0900

    UI作成

--- RakuIcibaRankAPILesson/ViewController.swift
+++ RakuIcibaRankAPILesson/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var resultTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func search(_ sender: Any) {
+    }
 }
 
